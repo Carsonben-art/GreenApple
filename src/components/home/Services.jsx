187c360ed21b38ed54802e27b3d3a4c5a63793e1
unknown <ben03carson@gmail.com>
2025-01-05 16:43:58 +0300

    update section colors

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -12,19 +12,19 @@ export default function ServicesSection() {
           <span className={styles.tag}>Reforestation</span>
           <h3 className={styles.title}>Tree Planting Events</h3>
           
-          <p>Hands-on tree planting events to restore natural habitats and enhance green spaces.</p>
+          <p className={styles.p}>Hands-on tree planting events to restore natural habitats and enhance green spaces.</p>
         </div>
         <div className={styles.serviceCard}>
           <Image src="/community.jpg" alt="Clean-up Drive" width={400} height={250} className={styles.image}/>
           <span className={styles.tag}>Community Service</span>
           <h3 className={styles.title}>Clean-up Drives</h3>
-          <p>Community clean-up drives aimed at combating pollution and promoting cleanliness.</p>
+          <p className={styles.p}>Community clean-up drives aimed at combating pollution and promoting cleanliness.</p>
         </div>
         <div className={styles.serviceCard}>
           <Image src="/education.jpg" alt="Education Programs" width={400} height={250} className={styles.image}/>
           <span className={styles.tag}>Education</span>
           <h3 className={styles.title}>Sustainability Programs</h3>
-          <p>Educational programs to promote sustainability and environmental stewardship.</p>
+          <p className={styles.p}>Educational programs to promote sustainability and environmental stewardship.</p>
         </div>
       </div>
     </section>
